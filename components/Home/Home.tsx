53fda6dfdc57cb590df70e8925f21252f4abef96
DaniLucano10 <olucano10@gmail.com>
2025-04-16 16:54:02 -0500

    Subiendo primera parte de la landing

---
+++ components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+import WhyChoose from './WhyChoose/WhyChoose'
+import AnalyticsFeature from './AnalyticsFeature/AnalyticsFeature'
+import Feature from './Feature/Feature'
+
+const Home = () => {
+    return (
+        <div className='overflow-hidden '>
+            <Hero />
+            <WhyChoose />
+            <AnalyticsFeature />
+            <Feature />
+        </div>
+    )
+}
+
+export default Home
