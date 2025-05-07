0a246202e9f5627c5133d40cfef52bcd1e8eb392
DaniLucano10 <olucano10@gmail.com>
2025-05-07 12:38:16 -0500

    agreagando url

--- constant/Constant.ts
+++ constant/Constant.ts
@@ -1,32 +1,68 @@
 export const navLinks = [
-    {
-        id: 1,
-        url: '#',
-        label: 'Home'
-    },
-    {
-        id: 2,
-        url: '#',
-        label: 'About'
-    },
-    {
-        id: 3,
-        url: '#',
-        label: 'Feature'
-    },
-    {
-        id: 4,
-        url: '#',
-        label: 'Testimonial'
-    },
-    {
-        id: 5,
-        url: '#',
-        label: 'Blog'
-    },
-    {
-        id: 6,
-        url: '#',
-        label: 'Contact'
-    },
-]
+  {
+    id: 1,
+    url: "#inicio",
+    label: "Inicio",
+  },
+  {
+    id: 2,
+    url: "#acerca-de",
+    label: "Nosotros",
+  },
+  {
+    id: 3,
+    url: "#caracteristicas",
+    label: "Caracteristicas",
+  },
+  {
+    id: 4,
+    url: "#testimonios",
+    label: "Testimonios",
+  },
+  {
+    id: 5,
+    url: "#planes",
+    label: "Planes",
+  },
+  {
+    id: 6,
+    url: "#contacto",
+    label: "Contacto",
+  },
+];
+
+export const cardsData = [
+  {
+    image: "/images/i1.png",
+    alt: "Crear cuenta gratuita con LideraTec",
+    title: "Crea tu cuenta gratuita y empieza hoy",
+    linkText: "Comienza a ganar con nuestra app",
+    description: "Regístrate sin costo y accede a funciones exclusivas.",
+    linkUrl: "https://www.lideratec.com/signup",
+  },
+  {
+    image: "/images/i2.png",
+    alt: "Monitoreo de análisis de usuario",
+    title: "Monitorea el análisis de usuarios de manera intuitiva",
+    linkText: "Registra tu tienda y controla el rendimiento",
+    description: "Obtén información detallada sobre el uso de tu aplicación.",
+    linkUrl: "https://www.lideratec.com/analytics",
+  },
+  {
+    image: "/images/i3.png",
+    alt: "Seguro y confiable",
+    title: "Seguridad garantizada",
+    linkText: "Obtén la aplicación y disfruta de tranquilidad",
+    description: "Tus datos y privacidad están completamente protegidos.",
+    linkUrl: "https://www.lideratec.com/security",
+  },
+  {
+    image: "/images/i4.png",
+    alt: "Atención al cliente rápida",
+    title: "Soporte al cliente excepcional",
+    linkText: "Obtén ayuda en tiempo real",
+    description:
+      "Nuestro equipo está disponible para ayudarte con cualquier duda.",
+    linkUrl: "https://www.lideratec.com/support",
+  },
+];
