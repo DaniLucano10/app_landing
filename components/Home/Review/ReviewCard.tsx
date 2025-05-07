221838f775f32c41ace27d995e685410ad176946
DaniLucano10 <olucano10@gmail.com>
2025-05-07 12:37:32 -0500

    Agregando section para mejorar seo

--- components/Home/Review/ReviewCard.tsx
+++ components/Home/Review/ReviewCard.tsx
@@ -5,37 +5,39 @@ import { FaQuoteLeft, FaStar } from "react-icons/fa";
 type Props = {
   name: string;
   image: string;
+  role: string;
+  testimonial: string;
 };
 
-const ReviewCard = ({ image, name }: Props) => {
+const ReviewCard = ({ image, name, role, testimonial }: Props) => {
   return (
     <div className="w-full md:w-[90%] relative mx-auto p-6 bg-white shadow-lg rounded-lg">
-      <div>
-        <FaQuoteLeft className="w-14 h-14 opacity-10 absolute top-8" />
-      </div>
+      <FaQuoteLeft className="w-14 h-14 opacity-10 absolute top-8" aria-hidden="true" />
+
       <div className="grid grid-cols-1 lg:grid-cols-5 gap-6 items-center">
         <div className="col-span-3 order-2 lg:order-1">
           <p className="mt-8 text-sm sm:text-base md:text-lg font-medium leading-[1.5rem] sm:leading-[1.8rem] md:leading-[2.5rem]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-            exercitationem amet ipsa voluptate obcaecati ducimus ullam eveniet
-            illum quidem cum?
+            {testimonial}
           </p>
-          <div className="flex items-center mt-6">
-            <FaStar className="text-yellow-600 h-6 w-6" />
-            <FaStar className="text-yellow-600 h-6 w-6" />
-            <FaStar className="text-yellow-600 h-6 w-6" />
-            <FaStar className="text-yellow-600 h-6 w-6" />
-            <FaStar className="text-yellow-600 h-6 w-6" />
+
+          <div className="flex items-center mt-6" aria-label="Calificación de 5 estrellas">
+            {[...Array(5)].map((_, i) => (
+              <FaStar key={i} className="text-yellow-600 h-6 w-6" />
+            ))}
           </div>
-          <h1 className="text-xl font-semibold mt-8">{name}</h1>
-          <p className="mt-2 text-lg text-gray-600 font-medium mb-6">
-            Fullstack Web Developer
-          </p>
+
+          <h3 className="text-xl font-semibold mt-8">{name}</h3>
+          <p className="mt-2 text-lg text-gray-600 font-medium mb-6">{role}</p>
         </div>
-        {/* Image */}
+
         <div className="col-span-2 mx-auto order-1 lg:order-2">
-            <Image src={image} alt={name} width={250} height={120} className="rounded-full"/>
+          <Image
+            src={image}
+            alt={`Foto de ${name}`}
+            width={250}
+            height={120}
+            className="rounded-full object-cover"
+          />
         </div>
       </div>
     </div>
