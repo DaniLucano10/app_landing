3ba56083ab70128fa8628d46306e7acb3623ec6a
DaniLucano10 <olucano10@gmail.com>
2025-05-09 12:25:50 -0500

    Actualizacion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
-
-export default nextConfig;
